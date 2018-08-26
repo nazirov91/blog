dd4b6ee4c8da6adcb2e53279395f532ed36c468d
Sardorjon Nazirov <sardor.nazirov@gmail.com>
2018-08-26 15:35:50 -0400

    Updated the github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => (
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/nazirov91"
+          href="https://github.com/nazirov91/blog"
           target="_blank"
           rel="noopener noreferrer"
         >
